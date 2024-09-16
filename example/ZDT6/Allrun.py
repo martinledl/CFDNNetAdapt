49b2904bcaae8ee3a015eb3152e98ac5cc559828
Martin Lédl <martin@code8.cz>
2024-09-16 11:40:19 +0200

    Add gitignore

--- example/ZDT6/Allrun.py
+++ example/ZDT6/Allrun.py
@@ -73,53 +73,54 @@ def dnnEvaluation(vars):
 
     return costOut
 
-# declare CFDNNetAdapt
-algorithm = CFDNNetAdapt()
+if __name__ == "__main__":
+    # declare CFDNNetAdapt
+    algorithm = CFDNNetAdapt()
 
-# problem specification
-algorithm.nPars = 2
-algorithm.nObjs = 2
-algorithm.nOuts = 2
-algorithm.mainDir = "01_algoRuns/"
-algorithm.smpDir = "00_prepData/"
-algorithm.prbDir = "ZDT6/"
-algorithm.dataNm = "10_platypusAllSolutions.dat"
-algorithm.minMax = ""
+    # problem specification
+    algorithm.nPars = 2
+    algorithm.nObjs = 2
+    algorithm.nOuts = 2
+    algorithm.mainDir = "01_algoRuns/"
+    algorithm.smpDir = "00_prepData/"
+    algorithm.prbDir = "ZDT6/"
+    algorithm.dataNm = "10_platypusAllSolutions.dat"
+    algorithm.minMax = ""
 
-# algorithm parameters
-algorithm.nSam = 1000
-algorithm.deltaNSams = [1000]
-algorithm.nNN = 4
-algorithm.minN = 2
-algorithm.maxN = 20
-algorithm.nHidLay = 3
-algorithm.tol = 1e-5
-algorithm.iMax = 200
-algorithm.dRN = 1
-algorithm.nComps = 16
-algorithm.nSeeds = 4
+    # algorithm parameters
+    algorithm.nSam = 1000
+    algorithm.deltaNSams = [1000]
+    algorithm.nNN = 4
+    algorithm.minN = 2
+    algorithm.maxN = 20
+    algorithm.nHidLay = 3
+    algorithm.tol = 1e-5
+    algorithm.iMax = 200
+    algorithm.dRN = 1
+    algorithm.nComps = 16
+    algorithm.nSeeds = 4
 
-# parameters for ANN training
-algorithm.trainPro = 75
-algorithm.valPro = 15
-algorithm.testPro = 10
-algorithm.kMax = 10000
-algorithm.rEStop = 1e-5
-algorithm.dnnVerbose = False
+    # parameters for ANN training
+    algorithm.trainPro = 75
+    algorithm.valPro = 15
+    algorithm.testPro = 10
+    algorithm.kMax = 10000
+    algorithm.rEStop = 1e-5
+    algorithm.dnnVerbose = False
 
-# parameters for MOP
-algorithm.pMin = 0.0
-algorithm.pMax = 1.0
-algorithm.offSize = 100
-algorithm.popSize = 100
-algorithm.nGens = 250
+    # parameters for MOP
+    algorithm.pMin = 0.0
+    algorithm.pMax = 1.0
+    algorithm.offSize = 100
+    algorithm.popSize = 100
+    algorithm.nGens = 250
 
-# evaluation funcs
-algorithm.dnnEvalFunc = dnnEvaluation
-algorithm.smpEvalFunc = smpEvaluation
+    # evaluation funcs
+    algorithm.dnnEvalFunc = dnnEvaluation
+    algorithm.smpEvalFunc = smpEvaluation
 
-# initialize
-algorithm.initialize()
+    # initialize
+    algorithm.initialize()
 
-# run
-algorithm.run()
+    # run
+    algorithm.run()
--- example/ZDT6/testRun.py
+++ example/ZDT6/testRun.py
@@ -10,7 +10,7 @@ import numpy as np
 import dill as pickle
 import multiprocessing
 from testFunctions import *
-from CFDNNetAdaptV4 import *
+from CFDNNetAdaptV3 import *
 
 # bounds
 pMin = 0.0
@@ -65,53 +65,54 @@ def dnnEvaluation(vars):
 
     return costOut
 
-# declare CFDNNetAdapt
-algorithm = CFDNNetAdapt()
+if __name__ == "__main__":
+    # declare CFDNNetAdapt
+    algorithm = CFDNNetAdapt()
 
-# problem specification
-algorithm.nPars = 2
-algorithm.nObjs = 2
-algorithm.nOuts = 2
-algorithm.mainDir = "01_algoRuns/"
-algorithm.smpDir = "00_prepData/"
-algorithm.prbDir = "ZDT6/"
-algorithm.dataNm = "10_platypusAllSolutions.dat"
-algorithm.minMax = ""
+    # problem specification
+    algorithm.nPars = 2
+    algorithm.nObjs = 2
+    algorithm.nOuts = 2
+    algorithm.mainDir = "01_algoRuns/"
+    algorithm.smpDir = "00_prepData/"
+    algorithm.prbDir = "ZDT6/"
+    algorithm.dataNm = "10_platypusAllSolutions.dat"
+    algorithm.minMax = ""
 
-# algorithm parameters
-algorithm.nSam = 4000
-algorithm.deltaNSams = [4000]
-algorithm.nNN = 1
-algorithm.minN = 2
-algorithm.maxN = 4
-algorithm.nHidLay = 3
-algorithm.tol = 1e-5
-algorithm.iMax = 4
-algorithm.dRN = 1
-algorithm.nComps = 1
-algorithm.nSeeds = 1
+    # algorithm parameters
+    algorithm.nSam = 4000
+    algorithm.deltaNSams = [4000]
+    algorithm.nNN = 1
+    algorithm.minN = 2
+    algorithm.maxN = 4
+    algorithm.nHidLay = 3
+    algorithm.tol = 1e-5
+    algorithm.iMax = 4
+    algorithm.dRN = 1
+    algorithm.nComps = 1
+    algorithm.nSeeds = 1
 
-# parameters for ANN training
-algorithm.trainPro = 75
-algorithm.valPro = 15
-algorithm.testPro = 10
-algorithm.kMax = 5
-algorithm.rEStop = 1e-2
-algorithm.dnnVerbose = True
+    # parameters for ANN training
+    algorithm.trainPro = 75
+    algorithm.valPro = 15
+    algorithm.testPro = 10
+    algorithm.kMax = 5
+    algorithm.rEStop = 1e-2
+    algorithm.dnnVerbose = True
 
-# parameters for MOP
-algorithm.pMin = 0.0
-algorithm.pMax = 1.0
-algorithm.offSize = 10
-algorithm.popSize = 10
-algorithm.nGens = 2
+    # parameters for MOP
+    algorithm.pMin = 0.0
+    algorithm.pMax = 1.0
+    algorithm.offSize = 10
+    algorithm.popSize = 10
+    algorithm.nGens = 2
 
-# evaluation funcs
-algorithm.dnnEvalFunc = dnnEvaluation
-algorithm.smpEvalFunc = smpEvaluation
+    # evaluation funcs
+    algorithm.dnnEvalFunc = dnnEvaluation
+    algorithm.smpEvalFunc = smpEvaluation
 
-# initialize
-algorithm.initialize()
+    # initialize
+    algorithm.initialize()
 
-# run
-algorithm.run()
+    # run
+    algorithm.run()
