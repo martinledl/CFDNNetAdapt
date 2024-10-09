cf298bef0e731d9c46f1d51a9d18ad720aa8d871
Martin Lédl <martin@code8.cz>
2024-10-09 16:29:46 +0200

    Change plotting of training runs

--- example/keras_implementation/compareTrainingRuns.py
+++ example/keras_implementation/compareTrainingRuns.py
@@ -22,19 +22,26 @@ def compare_training_runs(histories):
 
 
 if __name__ == "__main__":
-    for i in range(1, 4):
-        with open(f'01_algoRuns/run_08/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
-            history1 = History(pickle.load(f), "tanh")
+    histories = []
+    for i in range(1, 8):
+        with open(f'01_algoRuns/run_{i:02d}/step_0001/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
+            history = pickle.load(f)
 
-        with open(f'01_algoRuns/run_09/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
-            history2 = History(pickle.load(f), "relu")
+        with open(f'01_algoRuns/run_{i:02d}/log.out') as f:
+            func = f.readlines()[30].split(' ')[-1]
 
-        with open(f'01_algoRuns/run_10/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
-            history3 = History(pickle.load(f), "sigmoid")
+        histories.append(History(history, func))
 
-        with open(f'01_algoRuns/run_11/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
-            history4 = History(pickle.load(f), "linear")
+        #with open(f'01_algoRuns/run_09/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
+        #    history2 = History(pickle.load(f), "relu")
 
-        compare_training_runs([history1, history2, history3, history4])
-        plt.show()
+        #with open(f'01_algoRuns/run_10/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
+        #    history3 = History(pickle.load(f), "sigmoid")
+
+        #with open(f'01_algoRuns/run_11/step_{i:04d}/2_16_16_16_2/training_history-000.pkl', 'rb') as f:
+        #    history4 = History(pickle.load(f), "linear")
+
+        #compare_training_runs([history1, history2, history3, history4])
+        compare_training_runs(histories)
+        plt.savefig(f'training_comparison.png')
         plt.close()
